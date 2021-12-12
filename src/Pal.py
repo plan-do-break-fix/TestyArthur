daff8ea61a5f550d15a41c75b3842efe081a6cb6
plan-do-break-fix <jeffdswan@outlook.com>
2021-12-11 21:49:25 -0600

    (refactor) replaced extract method in Pal with proc and parse

--- src/Pal.py
+++ src/Pal.py
@@ -2,23 +2,28 @@
 
 import importlib
 from inspect import signature
-from typing import List, Union
+from typing import Dict, List, Tuple, Union
 from unittest import Class, TestCase
 
 class Pal:
 
-    def __init__(self, testdoc: dict):
+    def __init__(self, testdoc: Dict):
 
-        self.candidates = []
+        _meta, _keys = testdoc["metadata"], list(testdoc["metadata"].keys())
+        self.outpath = self.parse_outpath(_meta["outpath"])
+        self.target = _meta["target"]
+        self.alias = _meta["alias"] if "alias" in _keys else _meta["target"].lower()
+        self.setup = True if "setup" in _keys else False
+        self.teardown = True if "teardown" in _keys else False
 
-        for _key in testdoc["metadata"]:
-            self.__setattr__(self, _key, testdoc["metadata"][_key])
+        self.tests = []
 
         self.assertions = {_m: self.arg_count(TestCase, _m) 
                            for _m in self.methods(TestCase)
                            if _m.startswith("assert")
-                           and not _m.endswith("_")
+                           and not _m.endswith("_")  # ignore TestCase.assert_()
                            }
+
         _class = self.class_by_name(self.target[0], self.target[1])
         self.methods = {_m: self.arg_count(_class, _m)
                         for _m in self.methods(_class)
@@ -29,13 +34,13 @@ class Pal:
             assertion = f"assert{assertion.lower()}"
         else:
             assertion = assertion.lower()
-        for _a in list(self.assertions.keys()):
+        for _a in list(self.assertions):
             if assertion == _a[0].lower():
                 return _a[0]
         return False
 
     def normalize_method(self, method_name: str) -> Union[str, bool]:
-        for _m in list(self.methods.keys()):
+        for _m in list(self.methods):
             if method_name.lower() == _m.lower():
                 return _m
         return False
@@ -56,108 +61,91 @@ class Pal:
                     in str(sig)[1:-1].split(",")
                     if _i.strip() != "self"])
 
-    def extract_values(self, data: object, known: dict=None) -> None:
+###
+# From Visio file 
+
+    def types_in_list(self, _list: List[object]) -> List[str]:
+        """Returns a list of distinct object types contained in _list."""
+        types = [str(type(_i)).split("'")[1] for _i in _list]
+        if len(types) > 1:
+            types = list(set(types))
+        return types
+
+    def n_known_values(self, known:dict) -> int:
+        """Returns the number of values in a known values dictionary."""
+        _n = 1 if known["method"] else 0
+        return _n + len(known["args"]) + len(known["assertion"])
+
+    def n_required_values(self, method_name: str, assertion: str) -> int:
+        """Returns number of values required to complete a known values dict for
+           a given normalized method, normalized assertion pair."""
+        # The method to test and the assertion itself make two
+        return 2 + self.assertions[assertion] + self.methods["method_name"] 
+
+    def proc(self, data: Dict, known: Dict=None):
+        if not data:
+            if self.validate(known):
+                self.tests.append(known)
         known = {} if not known else known
         if type(data) == dict:
-            # determine value type of key, add to known, recursive call on all values
-            _keys = list(data.keys())
-            for _k in _keys:
-                _assertion = self.normalize_assertion(_k)
-                if _assertion:
-                    if not known["assertion"]:
-                        known["assertion"] = [_assertion]
-                    else:
-                        # found multiple assertions for single test
-                        raise RuntimeError
-                else:
-                    _method = self.normalize_method(_k)
-                    if _method:
-                        if not known["method"]:
-                            known["method"] = _method
-                        else:
-                            # found two method names belonging to target
-                            raise RuntimeError
-                    else:
-                        # assume it is first arg to pass to method being tested
-                        if not known["args"]:
-                            known["args"] = [_k]
-                self.candidates.append(self.extract_values(data[_k], known))
-        elif type(data) == str:
-            self.candidates.append(self.values_from_strings([data], known))
+            self.proc_dict(data, known)
         elif type(data) == list:
-            if all([type(_i) in [list, dict] for _i in data]):
-                # recursive call on all values
-                for _i in data:
-                    self.candidates.append(self.extract_values(_i, known))
-            # determine if this is a terminal call
-            if all([type(_i) == str for _i in data]):
-                # terminal - process values, add to output
-                for _i in data:
-                    self.candidates.append(self.values_from_strings(_i, known))
-            else:
-                # add strings to known, recursive call on others
-                strings = [_i for _i in data if type(_i) == str]
-                others = [_i for _i in data if type(_i) != str]
-                known = self.values_from_strings(strings, known)
-                for _i in others:
-                    self.candidates.append(self.extract_values(_i, known))
+            self.proc_list(data, known)
+        elif type(data) == str:
+            self.proc([data,], known)
+
+    def proc_dict(self, data: Dict, known: Dict) -> None:
+        if len(data.keys()) > 1:
+        # split up job if multiple keys
+            for _k in data.keys():
+                self.proc(dict(_k = data[_k]), known)
+        elif len(data.keys()) == 1:
+            known = self.parse_values(list(data)[0], known)
+            self.proc(data[list(data)[0]], known)
+
+    def proc_list(self, data: List, known: Dict) -> None:
+        _types = self.types_in_list(data)
+        if "str" not in _types:
+            for _d in data:
+                self.proc(_d, known)
+        if len(_types) > 1 and "str" in _types:
+            values = [_d for _d in data if type(_d) == str]
+            data = [_d for _d in data if type(_d) != str]
+            known = self.parse_values(values, known)
+            self.proc(data, known)
+
+    def parse_values(self, values: List[str], known: Dict) -> Dict:
+        """Adds all values to known and returns.
+           * len(values) expected to equal n_required_values(known)"""
+        if not known["method"]:
+            for _i, _v in enumerate(values):
+                if self.normalize_method(_v):
+                    known["method"] = [self.normalize_method(_v),]
+                    return self.parse_values(values, known)
+        if not known["assertion"]:
+            for _i, _v in enumerate(values):
+                if self.normalize_assertion(_v):
+                    known["assertion"] = [self.normalize_assertion(_v),]
+                    return self.parse_values(values, known)
+        method_values_needed = (self.methods[known["method"][0]] + 1 - len(known["method"]))
+        if method_values_needed:
+            known["method"] += values[:method_values_needed]
+            if values[method_values_needed:] == []:
+                return known
+            return self.parse_values(values[method_values_needed:], known)
+        assert_values_needed = (self.assertions[known["assertion"][0]] + 1 - len(known["assertion"]))
+        if assert_values_needed:
+            known["assertion"] += values[:assert_values_needed]
+            if values[assert_values_needed:] == []:
+                return known
+            return self.parse_values(values[assert_values_needed:], known)
+
+        
+
+
+    def split_values(self, values: List[str], sublen: int) -> List[List[str]]:
+        """Return len(values)/n lists of every n strings in values.
+           * len(values) expected to be a multiple of n_required_values(known)"""
+        pass  # TODO
+                    #values = values[:_i][_i+1:]
             
-    def values_from_strings(self, values: List[str], known: dict) -> dict:
-        added = []  # job finished if len(added) = len(values); used as filter on recursive calls
-        if not known ["method"]:
-            for _index, _value in enumerate(values):
-                _method = self.normalize_method(_value)
-                if _method:
-                    known["method"] = [_method]
-                    added.append(_method)
-                    if self.methods[_method] > 1:
-                        for _i in range(1, self.methods[_method]):
-                            if not known["args"]:
-                                known["args"] = []
-                            known["args"].append(values[_index + _i])
-                            added.append(values[_index + _i])
-                            break
-                    break
-            if len(added) == len(values):
-                return known
-            else:
-                self.values_from_strings([_v for _v in values if _v not in added])
-        elif not known["assertion"]:
-            for _index, _value in enumerate(values):
-                _assertion = self.normalize_assertion(_value)
-                if _assertion:
-                    known["assertion"] = [_assertion]
-                    added.append(_assertion)
-                    if self.assertions[_assertion] > 1:
-                        for _i in range(1, self.assertions[_assertion]):
-                            known["assertion"].append(values[_index + _i])
-                            added.append(values[_index + _i])
-                            break
-                    break
-            if len(added) == len(values):
-                return known
-            else:
-                self.values_from_strings([_v for _v in values if _v not in added])
-        # check if all values are missing assertion arguments
-        if  int(self.assertions[known["assertion"][0]]
-                + 1
-                - len(known["assertion"]))\
-                == len(values):
-                known["assertion"] += values
-                return known
-        # values must be arguments for method being tested
-        if not known["args"]:
-            known["args"] = []
-        if  int(self.methods[known["method"]]
-                + 1
-                - len(known["args"]))\
-                == len(values):
-                known["args"] += values
-                return known
-
-
-
-
-
-
-
